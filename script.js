f5907c8d85e3da8bb3767d90bd77684199ddd826
zwehtetpaing-dev <zwehtetpaing.dev@gmail.com>
2025-08-04 23:09:07 +0700

    Upgrade script.js

--- script.js
+++ script.js
@@ -1,51 +1,94 @@
-let clickCount = 0;
-
-function createNewTask() {
-    const input = document.getElementById("task-input");
-
-    if (input.value === "") {
-        return;
-    }
-
-    clickCount++;
-
-    const div = document.createElement("div");
-    const checkbox = document.createElement("input");
-    const label = document.createElement("label");
-    const button = document.createElement("button");
-
-    div.id = `task-${clickCount}`;
-    checkbox.type = "checkbox";
-
-    button.textContent = "Delete";
-    button.id = `deleteButton-${clickCount}`;
-    // Add button onclick function that produce the task number everytime it has been clicked. 
-    // Then put the number of the task as argument for deleteTask() function.
-    button.addEventListener("click", function() {
-        let num = [];
-        length = this.id.length
-        for (let i = length - 1; i >= 0; i--) {
-            if (this.id[i] === "-") {
-                break;
-            } else {
-                num.push(this.id[i]);
-            }
+// Make task left on HTML even after reload.
+document.addEventListener('DOMContentLoaded', function() {
+    let tasks = JSON.parse(sessionStorage.getItem("tasks"));
+    if (tasks === null) {
+        console.log("hello world");
+    }else {
+        for (let i = 0; i < tasks.length; i++) {
+            container.innerHTML += tasks[i].tags;
         }
-        num.reverse();
-        let tmpNum = Number(num.join(""));
-        deleteTask(tmpNum);
-    });
+    }
+});
 
-    label.textContent = input.value;
+let taskCounter = 0;
+const taskInput = document.getElementById("taskInput");
+const createBtn = document.getElementById("createBtn");
+const container = document.getElementById("container");
 
-    div.appendChild(checkbox);
-    div.appendChild(label);
-    div.appendChild(button);
-    document.body.appendChild(div);
+createBtn.addEventListener("click", () => {
+    if (taskInput.value === "") {
+        return;
+    }else {
+        taskCounter++;
+
+        // Creating object
+        let component = {
+            id: taskCounter,
+            taskId: `task${taskCounter}`,
+            delBtnId: `delBtn${taskCounter}`,
+            added: false,
+            tags: `
+                <div id="task${taskCounter}">
+                    <input type="checkbox">
+                    <label>${taskInput.value}</label>
+                    <button id="delBtn${taskCounter}" onclick="deleteTask()">Delete</button>
+                </div>
+            `
+        }
+        // Store in session about componenet object
+        storeInSession(component);
+
+        // Create new tasks
+        createTask();
+    }
+})
+
+// Store objs in the session
+function storeInSession(obj) {
+    let tasks = JSON.parse(sessionStorage.getItem("tasks"));
+    if (tasks === null) {
+        let arr = [];
+        arr.push(obj);
+        sessionStorage.setItem("tasks", JSON.stringify(arr));
+    } else {
+        tasks.push(obj);
+        sessionStorage.setItem("tasks", JSON.stringify(tasks));
+    }
 }
 
+// Create new tasks function
+function createTask() {
+    let tasks = JSON.parse(sessionStorage.getItem("tasks"));
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].added === false) {
+            tasks[i].added = true;
+            container.innerHTML += tasks[i].tags;
+        }
+    }
+    sessionStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
-function deleteTask(taskNo) {
-    const tag = document.getElementById(`task-${taskNo}`);
-    tag.remove();
+// Delete task
+function deleteTask() {
+    const childId = event.target.id;
+    const tasks = JSON.parse(sessionStorage.getItem("tasks"));
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].delBtnId === childId) {
+            const child = document.getElementById(childId);
+            const parent = child.parentElement;
+            document.getElementById(parent.id).remove();
+            tasks.splice(i, 1);
+            sessionStorage.setItem("tasks", JSON.stringify(tasks));
+        }
+    }
+
+}
+
+function test() {
+    let tasks = JSON.parse(sessionStorage.getItem("tasks"));
+    console.log(tasks);
+}
+
+function clearSession() {
+    sessionStorage.clear();
 }
